9f7e41bc5c9e9c6f8cbd6f580d3738cf3614c358
Norbert CU <norbertcu@gmail.com>
2020-04-01 23:07:24 -0600

    Add Cloudinary support.

---
+++ app/utils/cloudinary.js
@@ -0,0 +1,10 @@
+const { CloudinaryAdapter } = require('@keystonejs/file-adapters');
+
+const cloudinaryAdapter = new CloudinaryAdapter({
+	cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+	apiKey: process.env.CLOUDINARY_KEY,
+	apiSecret: process.env.CLOUDINARY_SECRET,
+	folder: 'omlsey',
+});
+
+module.exports.cloudinaryAdapter = cloudinaryAdapter;
