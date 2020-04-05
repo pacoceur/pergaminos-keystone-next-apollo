06b10e75b8bcc882767ffe1ad6c98ea61ea7df0e
pacoceur <pacoceur@gmail.com>
2020-04-04 20:06:40 -0600

    user authentication

--- index.js
+++ index.js
@@ -28,14 +28,14 @@ const PostList = require('./lists/Post.js');
 keystone.createList('Post', PostList);
 
 // User Authentication
-/*const authStrategy = keystone.createAuthStrategy({
+const authStrategy = keystone.createAuthStrategy({
   type: PasswordAuthStrategy,
   list: 'User',
   config: {
     identityField: 'username',
     secretField: 'password',
   },
-});*/
+});
 
 // Exports
 module.exports = {
@@ -44,7 +44,7 @@ module.exports = {
     new GraphQLApp(),
     new AdminUIApp({ 
       enableDefaultRoute: false,
-      // authStrategy,
+      authStrategy,
     }),
     new NextApp({ dir: 'app' }),
   ],
